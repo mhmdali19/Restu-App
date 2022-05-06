88e0a4d86c4aaa8912ec463bbdcedc9c03d88f9c
khodor0098 <khodor0097@gmail.com>
2022-05-06 22:04:36 +0300

    "hello world"

---
+++ app/Http/Controllers/Pagecontroller.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Pagecontroller extends Controller
+{
+    //
+
+    public function index(){
+    
+       return view('pages.index');
+    }
+
+    public function about(){
+
+        return view('pages.about');
+    }
+
+    public function feedback(){
+    
+        return view('pages.feedback');
+    }
+
+    public function menu(){
+    
+        return view('pages.menu');
+    }
+
+    public function reservation(){
+    
+        return view('pages.reservation');
+    }
+}
