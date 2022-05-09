d71e32502ba32e93a03f5cb72d27ccc82c9ec6d5
khodor0098 <khodor0097@gmail.com>
2022-05-09 19:55:53 +0300

    fixid

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -22,15 +22,33 @@
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
         <a class="navbar-brand" href="index.html">Taste.<span>it</span></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+            aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="oi oi-menu"></span> Menu
         </button>
 
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
-                <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
-                <li class="nav-item active"><a href="/menu" class="nav-link">Menu</a></li>
+                {{-- @auth
+                    @if (Auth::user()->role == 'super_admin')
+
+                    @endif
+
+                @endauth --}}
+                <li class="nav-item
+                @if (Request::is('/')) active @endif
+                "><a
+                        href="/" class="nav-link">Home</a></li>
+                <li class="nav-item @if (Request::is('about')) active @endif"><a href="/about"
+                        class="nav-link">About</a></li>
+                <li
+                    class="
+                    nav-item
+                    @if (Request::is('menu')) active @endif
+                    ">
+                    <a href="/menu" class="nav-link">Menu
+                    </a>
+                </li>
                 <li class="nav-item"><a href="/reservation" class="nav-link">Reservation</a></li>
                 <li class="nav-item"><a href="/feedback" class="nav-link">feedback</a></li>
             </ul>
