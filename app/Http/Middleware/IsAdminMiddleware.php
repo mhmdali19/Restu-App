ea3deb3419cc79797edccfb1cef0d117247ccc01
Mohammad Ali <mahdifd313@gmail.com>
2022-05-17 12:07:58 +0300

    another milka

---
+++ app/Http/Middleware/IsAdminMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Enums\UserRole;
+
+class IsAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+
+        if(!Auth::check()){
+            return redirect('/');
+        }
+
+        $user = Auth::user();
+
+        if($user->role != UserRole::SUPER_ADMIN){
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\RegisterController;
-
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +13,7 @@ use App\Http\Controllers\Auth\RegisterController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 // Home Index
 Route::resource('/', 'App\Http\Controllers\indexcontroller');
@@ -24,14 +22,14 @@ Route::resource('/', 'App\Http\Controllers\indexcontroller');
 Route::resource('/about', 'App\Http\Controllers\aboutcontroller');
 
 // Menu
-Route::resource('/menu','App\Http\Controllers\MenuController');
+Route::resource('/menu', 'App\Http\Controllers\MenuController');
 
 // Reservation
 Route::resource('/reservation', 'App\Http\Controllers\reservationcontroller');
 Route::post('/newres', 'App\Http\Controllers\reservationController@store');
 
 // Feedback
-Route::resource('feedback','App\Http\Controllers\feedbackController');
+Route::resource('feedback', 'App\Http\Controllers\feedbackController');
 Route::post('/newfeedback', 'App\Http\Controllers\feedbackController@store');
 //
 //
@@ -50,14 +48,9 @@ Route::post('/newfeedback', 'App\Http\Controllers\feedbackController@store');
 //////////
 // Admin Feedback
 
+Auth::routes(['register' => false]);
 
-
-
-Auth::routes(['register'=> false]);
-
-
-    Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register')->middleware('isAdmin');
-
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register')->middleware('isAdmin');
 
 Route::group(['middleware' => ['isAdmin']], function () {
     Route::post('register', [RegisterController::class, 'register']);
@@ -65,30 +58,30 @@ Route::group(['middleware' => ['isAdmin']], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('adminHome', 'App\Http\Controllers\HomeController@adminView')->name('admin.view');
 
-    Route::resource('/feedbackview','App\Http\Controllers\adminfeedbackController');
+    Route::resource('/feedbackview', 'App\Http\Controllers\adminfeedbackController');
 
 // Admin Reservation
-Route::resource('/reservationview','App\Http\Controllers\adminreservationController');
+    Route::resource('/reservationview', 'App\Http\Controllers\adminreservationController');
 
 // Admin Product
-Route::resource('/product','App\Http\Controllers\ProductController');
-Route::post('/newproduct', 'App\Http\Controllers\ProductController@store');
-Route::get('delete/{id}','App\Http\Controllers\ProductController@destroy');
+    Route::resource('/product', 'App\Http\Controllers\ProductController');
+    Route::post('/newproduct', 'App\Http\Controllers\ProductController@store');
+    Route::get('delete/{id}', 'App\Http\Controllers\ProductController@destroy');
 
 //Admin offers
-Route::resource('/offers','App\Http\Controllers\offersController');
-Route::post('/newoffer', 'App\Http\Controllers\offersController@store');
-Route::get('delete1/{id}','App\Http\Controllers\offersController@destroy');
+    Route::resource('/offers', 'App\Http\Controllers\offersController');
+    Route::post('/newoffer', 'App\Http\Controllers\offersController@store');
+    Route::get('delete1/{id}', 'App\Http\Controllers\offersController@destroy');
 
 //Admin events
-Route::resource('/events','App\Http\Controllers\eventsController');
-Route::post('/newevent', 'App\Http\Controllers\eventsController@store');
-Route::get('delete/{id}','App\Http\Controllers\eventsController@destroy');
+    Route::resource('/events', 'App\Http\Controllers\eventsController');
+    Route::post('/newevent', 'App\Http\Controllers\eventsController@store');
+    Route::get('delete/{id}', 'App\Http\Controllers\eventsController@destroy');
 });
 
 
+// Route::group(['middleware' => ['forAdmins']], function () {
 
 
 
-
-
+// }
